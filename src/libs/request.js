bad52aa62b3a93c19836570ee040360fd5e69068
liuyadu <515608851@qq.com>
2018-12-13 11:44:38 +0800

    修改

--- src/libs/request.js
+++ src/libs/request.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import qs from "qs";
+import axios from 'axios'
+import qs from "qs"
 import config from '@/config'
 import {getToken} from '@/libs/util'
 
@@ -10,17 +10,27 @@ const service = axios.create({
   timeout: 5000  // 请求超时时间
 });
 /****** request拦截器==>对请求参数做处理 ******/
-service.interceptors.request.use(config => {
-  config.method === 'post'
-    ? config.data = qs.stringify({...config.data})
-    : config.params = {...config.params};
-  config.headers['Content-Type'] = 'application/x-www-form-urlencoded';
-  if (getToken()) {
-    config.headers['Authorization'] = 'Bearer ' + getToken();
+service.interceptors.request.use(
+  config => {
+    config.method === 'post'
+      ? config.data = qs.stringify({...config.data})
+      : config.params = {...config.params};
+    config.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+    if (getToken()) {
+      config.headers['Authorization'] = 'Bearer ' + getToken();
+    }
+    return config;
+  },
+  response => {
+    if (response.data.code === 0) {   //服务端定义的响应code码为0时请求成功
+      return Promise.resolve(response.data) //使用Promise.resolve 正常响应
+    }else {
+      return Promise.reject(response.data)
+    }
+  }, error => {  //请求错误处理
+    Promise.reject(error)
   }
-  return config;
-}, error => {  //请求错误处理
-  Promise.reject(error)
-});
+)
+;
 
 export default service
