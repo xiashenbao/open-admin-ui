32b6578b5065dab655e3c0a9f2e225dbb7e9711a
liu.yadu <515608851@qq.com>
2019-01-09 00:41:47 +0800

    修改样式

--- src/api/role.js
+++ src/api/role.js
@@ -35,3 +35,42 @@ export const removeRole = ({roleId}) => {
     method: 'post'
   })
 }
+
+
+
+export const roleGrantApi = ({roleId,apiIds}) => {
+  const data= {
+    roleId:roleId,
+    apiIds:apiIds
+  }
+  return request({
+    url: 'base/role/grant/api',
+    data,
+    method: 'post'
+  })
+}
+
+
+export const roleGrantMenu = ({roleId,menuIds}) => {
+  const data= {
+    roleId:roleId,
+    menuIds:menuIds
+  }
+  return request({
+    url: 'base/role/grant/menu',
+    data,
+    method: 'post'
+  })
+}
+
+export const roleGrantAction = ({roleId,actionIds}) => {
+  const data= {
+    roleId:roleId,
+    actionIds:actionIds
+  }
+  return request({
+    url: 'base/role/grant/action',
+    data,
+    method: 'post'
+  })
+}
